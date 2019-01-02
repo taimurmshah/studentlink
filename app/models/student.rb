c5f095b9c6783542bafe29c085ee7aad4c851315
github username <github email address>
2019-01-02 14:24:10 -0500

    making progress, just figured out seeds

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,6 @@ class Student < ApplicationRecord
   has_many :courses, through: :seats
 
   validates :username, uniqueness: true
+  validates :username, presence: true
+  validates :name, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.destroy_all
+Course.destroy_all
+Teacher.destroy_all
+
+
+students = [
+  {name: "Djavan Guy", username: "Beefdj"},
+  {name: "Leo Folla", username: "Theslikestofall"},
+  {name: "Mahdi Ladjouzi", username: "Frostfire"},
+  {name: "Taimur Shah", username: "donshah"}
+]
+
+students.each do |student|
+  Student.create(student)
+end
+
+teachers = [
+  {name: "Douglas Otis"},
+  {name: "Stephen Carroll"},
+  {name: "Tim Ferriss"},
+  {name: "Patricia Morris"}
+]
+
+
+
+teachers.each do |teacher|
+  Teacher.create(teacher)
+end
+
+
+courses = [
+  {title: "Music Theory", teacher_id: Teacher.all.sample[:id]},
+  {title: "Advanced Rhetoric", teacher_id: Teacher.all.sample[:id]},
+  {title: "Electromagnetism", teacher_id: Teacher.all.sample[:id]},
+  {title: "How to Mack 101", teacher_id: Teacher.all.sample[:id]}
+]
+
+courses.each do |course|
+  Course.create(course)
+end
